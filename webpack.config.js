fa36dcf39c0027f5b58a6a2f2cdbe519713f003a
rhiokim <rhio.kim@gmail.com>
2018-04-09 00:22:06 +0900

    feat: add run-scripts for clear old dist

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    filename: '[name]/[name].js',
     libraryTarget: 'commonjs2'
   },
   module: {
