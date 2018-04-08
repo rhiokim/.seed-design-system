f02857a363c055b140639280a26bb90323f8e35d
rhiokim <rhio.kim@gmail.com>
2018-04-08 20:53:30 +0900

    feat: add sample components

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,16 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: {
-    Button: './src/Button.js',
-    Panel: './src/Panel.js'
+    Button: './src/Button/Button.js',
+    Panel: './src/Card/Panel.js',
+    Card: './src/Card/Card.js',
+    Image: './src/Image/Image.js',
+    Badge: './src/Badge/Badge.js',
+    Input: './src/Input/Input.js',
+    Modal: './src/Modal/Modal.js'
   },
   output: {
-    path: path.resolve(__dirname, 'lib'),
+    path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
     libraryTarget: 'commonjs2'
   },
